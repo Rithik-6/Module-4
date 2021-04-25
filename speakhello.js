ee0ec859d95dc98070ef6910d9adbaa493576a3e
Rithik-6 <80526070+Rithik-6@users.noreply.github.com>
2021-04-25 13:39:09 +0530

    Update speakhello.js

--- speakhello.js
+++ speakhello.js
@@ -1,3 +1,15 @@
+// ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
 Hello Yaakov
 Good Bye John
 Good Bye Jen
@@ -16,7 +28,7 @@ WARNING!!! WARNING!!!
 */
 
 
-(function () {// missing closing }
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -32,4 +44,3 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
-
